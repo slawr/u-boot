4bae0421ee13fced563939035928af6368302b01
Tetsuyuki Kobayashi <koba@kmckk.co.jp>
2013-01-10 17:46:35 +0000

    arm: armv7: add compile option -mno-unaligned-access if available
    Recent compiler generates unaligned memory access in armv7 default.
    But current U-Boot does not allow unaligned memory access, so it causes
    data abort exception.
    This patch add compile option "-mno-unaligned-access" if it is available.
    
    Signed-off-by: Tetsuyuki Kobayashi <koba@kmckk.co.jp>

--- arch/arm/cpu/armv7/config.mk
+++ arch/arm/cpu/armv7/config.mk
@@ -24,6 +24,8 @@ PLATFORM_RELFLAGS += -fno-common -ffixed-r8 -msoft-float
 
 # Make ARMv5 to allow more compilers to work, even though its v7a.
 PLATFORM_CPPFLAGS += -march=armv5
+PLATFORM_CPPFLAGS +=$(call cc-option, -mno-unaligned-access,)
+
 # =========================================================================
 #
 # Supply options according to compiler version
